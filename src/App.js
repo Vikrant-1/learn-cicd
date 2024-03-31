f9470e04fa84ccc04d54f4ebe35fd09ba3caefbd
vikrant22 <vikrant@usefreebird.com>
2024-03-31 18:43:59 +0530

    title change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          learn git hub actions
         </a>
       </header>
     </div>
